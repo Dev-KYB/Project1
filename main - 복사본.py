18952da80228bfba42fcc96392ff6230a35d54c3
kyv1216 <kyv1216@naver.com>
2023-04-03 17:53:20 +0900

    커밋 제곱

--- main - 복사본.py
+++ main - 복사본.py
@@ -11,7 +11,12 @@ def gob_func(n1, n2):
 def nanu_func(n1, n2):
     return n1/n2
 
+def gob2_func(n3,n4):
+    return n3**n4
+
 num1,num2, result=100,200,0
+num3,num4, result2=10,2,0
+
 
 result = add_func(num1, num2)
 print(num1, '+', num2, '=', result)
@@ -24,3 +29,6 @@ print(num1, '*', num2, '=', result)
 
 result = nanu_func(num1, num2)
 print(num1, '/', num2, '=', result)
+
+result = gob2_func(num3,num4)
+print(num3, '**', num4, '=', result)
